d1a303fbbe1413a3a64a6ce811f6473e41747d64
bizlemdev <bizlemdev@gmail.com>
2017-06-16 17:03:13 +0530

    Update HelloWorldController.java

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -24,6 +24,7 @@ try{
 	        text = result.getString("resolvedQuery");
 	  JSONObject originalRequest = objS.getJSONObject("originalRequest");
 	        source = result.getString("source");
+	        text="I love India";
            
             
 	    
