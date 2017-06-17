38dead48511315d20a5c186fcd145d8734f2883a
bizlemdev <bizlemdev@gmail.com>
2017-06-17 13:43:21 +0530

    Update HelloWorldController.java

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -6,6 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @Controller
 @RequestMapping("/webhook")
@@ -34,7 +42,7 @@ try{
 	      //  int rescode=authorize();
 	    //    rescode_str=String.valueOf(rescode);//Now it will return "10"  
 	HelloWorldController hwc=new HelloWorldController();
-	//String res=hwc.authorize(obj);
+	String res=hwc.authorize(obj);
            
             
 	    
@@ -46,6 +54,43 @@ try{
 	       return  "{speech: "+text+",displayText: "+text+", source: biz-webhook-sample}";
 	       //return  "{speech: "+text+" source "+source+",displayText: "+text+", source: biz-webhook-sample}";
 	    }
+	
+	public String authorize(String obj){
+  
+		 StringBuilder result=null;
+                 int statusCode = 0;
+		 try {
+            int BUFFER_SIZE = 4096;
+          URL url = new URL("http://34.196.246.23:8101/MessagingToolController?webhookrespo=hi");
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+
+            http.setRequestMethod("POST");
+            http.setRequestProperty("Content-Type", "ext/html");
+      
+             http.setUseCaches(false);
+        
+            http.setDoOutput(true);
+   
+       
+         
+            statusCode = http.getResponseCode();
+            System.out.println("code=======" + statusCode);
+            String newLine = System.getProperty("line.separator");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(http.getInputStream()));
+            result = new StringBuilder();
+            String line;
+            boolean flag = false;
+            while ((line = reader.readLine()) != null) {
+                result.append(flag ? newLine : "").append(line);
+                flag = true;
+            }
+            System.out.println(result);
+        } catch (Exception e) {
+            System.out.println("error  " + e.getMessage());
+        }
+               //  return result;
+                 return result;
+	}
 	  
 	
 }
